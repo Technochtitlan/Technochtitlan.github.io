801b37ad714d937ce6bf08c8a3a9ed601580d025
dbautistav <david@opi.la>
2015-02-14 18:04:37 -0600

    Initial commit (yo)

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var gulp = require('gulp');
+var deploy = require('gulp-gh-pages');
+
+gulp.paths = {
+  src: 'src',
+  dist: 'dist',
+  tmp: '.tmp',
+  e2e: 'e2e'
+};
+
+var deployOptions = {};
+
+require('require-dir')('./gulp');
+
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
+
+gulp.task('deploy', function () {
+  return gulp.src('./dist/**/*')
+    .pipe(deploy(deployOptions));
+});
---
+++ src/components/navbar/navbar.controller.coffee
@@ -0,0 +1,3 @@
+angular.module "technochtitlan"
+  .controller "NavbarCtrl", ($scope) ->
+    $scope.date = new Date()
